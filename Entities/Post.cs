8bdd0560bd58d8a2ecd25e6c670c9fc1ac939170
Renato de Azevedo <renatoads1@gmail.com>
2020-04-13 11:26:32 -0300

    frist commit in the project post

---
+++ Entities/Post.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Posts.Entities
+{
+    class Post
+    {
+        public DateTime Data { get; set; }
+        public string Title { get; set; }
+        public string comments { get; set; }
+        public int likes { get; set; }
+        /*como tem muitos comentarios para cada post
+        instanciaremos uma lista de comentarios    */
+
+        public List<Comment> comentarios { get; set; } = new List<Comment>();
+
+        public Post()
+        {
+        }
+
+        public Post(DateTime data, string title, string comments, int likes)
+        {
+            Data = data;
+            Title = title;
+            this.comments = comments;
+            this.likes = likes;
+        }
+        public void AddComment(Comment com) {
+            comentarios.Add(com);        
+        }
+        public void RemoveComment(Comment com)
+        {
+            comentarios.Remove(com);
+        }
+        public override string ToString()
+        {
+            //usando o string Builder da clase system.text
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(Data.ToString());
+            sb.AppendLine(Title);
+            foreach (Comment com in comentarios) {
+                sb.AppendLine(com.Text);
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Posts.Entities;
+
+
+namespace Posts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("escreva o comentario 1");
+            Comment c1 = new Comment(Console.ReadLine().ToString());
+            Console.WriteLine("escreva o comentario 2");
+            Comment c2 = new Comment(Console.ReadLine().ToString());
+            Post post1 = new Post(DateTime.Parse("11/06/1982"),"titulo1","comentario1",12);
+            post1.AddComment(c1);
+            post1.AddComment(c2);
+
+            Console.WriteLine(post1);
+        }
+    }
+}
